92f8dbf763c8a34f9e99988a604958cd156309b6
ILUNOSHA~ <dimakorolev2000@gmail.com>
2023-10-05 16:02:27 +0300

    flake8

--- aaa_csv.py
+++ aaa_csv.py
@@ -3,31 +3,35 @@ import csv
 
 def file_open(path: str) -> list:
     """
-    Opens a CSV file at the given path and returns its contents as a list of lists.
+    Opens a CSV file and returns its contents as a list of lists.
 
-    :param path: A string representing the file path of the CSV file to be opened.
+    :param path: A string representing the file path of the
+    CSV file to be opened.
     :return: A list of lists representing the contents of the CSV file.
     """
-    with open(path, 'r', encoding='utf-8') as file:
-        file = csv.reader(file, delimiter=';')
+    with open(path, 'r', encoding='utf-8') as f:
+        file = csv.reader(f, delimiter=';')
         my_list = [row for row in file]
     return my_list
 
 
 def get_data(path: str) -> dict:
     """
-    Parses a CSV file containing information about employees and returns a dictionary
+    Parses a CSV file containing information
+    about employees and returns a dictionary
     containing information about departments.
 
-    :param path: A string representing the file path of the CSV file to be parsed.
+    :param path: A string representing the file path of the
+    CSV file to be parsed.
     :return: A dictionary containing information about departments.
     """
     data = file_open(path)[1:]
-    departments = {employer[1]: [set(), 1e6, 0, 0, 0] for employer in data}
+    departments = {employer[1]: [set(), float('inf'), 0, 0, 0.0]
+                   for employer in data}
     for row in data:
         departments[row[1]][0].add(row[2])
-        departments[row[1]][1] = float(row[5]) if float(row[5]) < departments[row[1]][1] else departments[row[1]][1]
-        departments[row[1]][2] = float(row[5]) if float(row[5]) > departments[row[1]][2] else departments[row[1]][2]
+        departments[row[1]][1] = min(float(row[5]), departments[row[1]][1])
+        departments[row[1]][2] = max(float(row[5]), departments[row[1]][2])
         departments[row[1]][3] += 1
         departments[row[1]][4] += float(row[5])
 
@@ -38,7 +42,8 @@ def find_departments(data: dict) -> None:
     """
     Prints a list of unique departments found in the given data.
 
-    :param data: A dictionary containing information about departments and their employees.
+    :param data: A dictionary containing information
+    about departments and their employees.
     :return: None
     """
     print('Департаменты и отделы:\n')
@@ -48,9 +53,11 @@ def find_departments(data: dict) -> None:
 
 def department_report(data: dict) -> None:
     """
-    Prints a summary report of departments, including the number of employees, minimum and maximum salaries, and average salary.
+    Prints a summary report of departments, including the number of
+    employees, minimum and maximum salaries, and average salary.
 
-    :param data: A dictionary containing information about departments and their employees.
+    :param data: A dictionary containing information
+    about departments and their employees.
     :return: None
     """
     print('Сводный отчёт по департаментам:\n')
@@ -64,26 +71,30 @@ def department_report(data: dict) -> None:
 
 def save_report(data: dict) -> None:
     """
-    Saves a summary report of departments to a CSV file, including the number of employees, minimum and maximum salaries,
+    Saves a summary report of departments to a CSV file,
+    including the number of employees, minimum and maximum salaries,
     and average salary.
 
-    :param data: A dictionary containing information about departments and their employees.
+    :param data: A dictionary containing information
+    about departments and their employees.
     :return: None
     """
     with open('department_report.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['Department', 'Number of Employees', 'Minimum Salary', 'Maximum Salary', 'Average Salary'])
+        writer.writerow(['Department', 'Number of Employees',
+                         'Minimum Salary', 'Maximum Salary', 'Average Salary'])
         for key, value in data.items():
-            writer.writerow([key, value[3], value[1], value[2], round(value[4] / value[3], 2)])
+            writer.writerow([key, value[3], value[1],
+                             value[2], round(value[4] / value[3], 2)])
     print('Отчёт сохранен в файл department_report.csv')
 
 
 if __name__ == '__main__':
     path = 'Corp_Summary.csv'
     choice = input('Выберите что хотите сделать:\n'
-                       '1. Вывести департаменты и все команды в нем\n'
-                       '2. Вывести сводный отчёт по департаментам\n'
-                       '3. Сохранить отчёт из пункта выше в формате csv\n')
+                   '1. Вывести департаменты и все команды в нем\n'
+                   '2. Вывести сводный отчёт по департаментам\n'
+                   '3. Сохранить отчёт из пункта выше в формате csv\n')
     data = get_data(path)
     while choice not in ['1', '2', '3']:
         choice = input('Выберите число от 1 до 3')
